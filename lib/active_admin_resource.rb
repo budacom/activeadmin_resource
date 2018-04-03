27c7fb3d2ed3d9b2443da5367e399b636780ee4e
Alejandro Echeverria <alejandro@echev.com>
2018-04-03 16:23:58 -0300

    fix(): refactor patches so they get loaded as a railtie

--- lib/active_admin_resource.rb
+++ lib/active_admin_resource.rb
@@ -1,6 +1,2 @@
 require 'active_admin_resource/base'
-require 'active_admin_resource/formtastic_addons_patch'
-require 'active_admin_resource/collection_patch'
-require 'active_admin_resource/connection_patch'
-
-ActiveResource::Connection.prepend(ConnectionExtensions)
+require 'active_admin_resource/railtie'
--- lib/active_admin_resource/formtastic_addons_patch.rb
+++ lib/active_admin_resource/formtastic_addons_patch.rb
@@ -1,24 +1,18 @@
-module ActiveAdmin
-  module Filters
-    module FormtasticAddons
-      alias :old_seems_searchable? :seems_searchable?
-      def seems_searchable?
-        return false #if ransacker?
-        #old_seems_searchable?
-      end
+module FormtasticAddonsExtensions
+  def seems_searchable?
+    return false
+  end
 
-      def klass
-        @object.try(:object).try(:klass)
-      end
+  def klass
+    @object.try(:object).try(:klass)
+  end
 
-      def ransacker?
-        klass.try(:_ransackers).try(:key?, method.to_s)
-      end
+  def ransacker?
+    klass.try(:_ransackers).try(:key?, method.to_s)
+  end
 
-      def scope?
-        context = Ransack::Context.for klass rescue nil
-        context.respond_to?(:ransackable_scope?) && context.ransackable_scope?(method.to_s, klass)
-      end
-    end
+  def scope?
+    context = Ransack::Context.for klass rescue nil
+    context.respond_to?(:ransackable_scope?) && context.ransackable_scope?(method.to_s, klass)
   end
 end
