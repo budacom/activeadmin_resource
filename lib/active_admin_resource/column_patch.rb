e2f90096a0eb3006a1b5c15c9d7a39c893bed66d
Alejandro Echeverria <alejandro@echev.com>
2018-04-18 15:49:55 -0300

    fix(): fixes column patch to work with activerecord

--- lib/active_admin_resource/column_patch.rb
+++ lib/active_admin_resource/column_patch.rb
@@ -3,7 +3,7 @@ module ColumnExtensions
     if @options.has_key?(:sortable)
       !!@options[:sortable]
     elsif @resource_class
-      @resource_class.column_names.map(&:name).include?(sort_column_name)
+      @resource_class.column_names.map { |c| c.is_a?(:String) ? c : c.name } .include?(sort_column_name)
     else
       @title.present?
     end
